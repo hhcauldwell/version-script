426cc4baa5bc9f850334cc063abb72bec3bcfb7d
Riley Cauldwell <riley.cauldwell@gmail.com>
2016-06-21 15:19:59 -0700

    Major changes to script and cleaned up a lot

--- index.js
+++ index.js
@@ -1,167 +1,40 @@
 #!/usr/bin/env node
 
-var assert = require('assert');
+// Nodejs modules.
 var util = require('util');
 
+// Npm modules.
 var Promise = require('bluebird');
 var yargs = require('yargs');
 var _ = require('lodash');
 var chalk = require('chalk');
-var yaml = require('yamljs');
 
+// Get command line arguments and options.
 var argv = yargs.argv;
 
-var scrapers = require('./lib/scrapers.js');
+// Load languages module.
+var languages = require('./lib/languages.js');
 
-var config = yaml.load('./config.yml');
+// Load images module.
+var images = require('./lib/images.js');
 
-function Language(opts) {
-  assert.equal(typeof opts, 'object');
-  assert.equal(typeof opts.id, 'string');
-  if (this instanceof Language) {
-    this.opts = opts;
-    this.id = opts.id;
-  } else {
-    return new Language(opts);
-  }
-}
-
-Language.getAll = function() {
-  return Language.get();
-}
-
-Language.get  = _.once(function() {
-  var ids = [
-    'c',
-    'clojure',
-    'coffeescript',
-    'cpp',
-    'csharp',
-    'erlang',
-    'go',
-    'haskell',
-    'java',
-    'javascript',
-    'mysql',
-    'objc',
-    'perl',
-    'php',
-    'python',
-    'python3',
-    'r',
-    'ruby',
-    'scala',
-    'swift',
-    'vb'
-  ];
-  return Promise.map(ids, function(id) {
-    return new Language({
-      id: id
-    });
-  });
-});
-
-var Docker = require('dockerode');
-
-var opts = {
-  docker: {
-    host: '127.0.0.1',
-    port: 2375
-  }
-};
-
-var docker = new Docker({
-  socketPath: '/var/run/docker.sock'
-  //host: opts.docker.host,
-  //port: opts.docker.port
-});
-
-function Image(opts) {
-  assert.equal(typeof opts, 'object');
-  assert.equal(typeof opts.id, 'string');
-  assert.equal(typeof opts.data, 'object');
-  if (this instanceof Image) {
-    this.id = opts.id;
-    this.data = opts.data;
-    this.opts = opts;
-  } else {
-    return new Image(opts);
-  }
-}
-
-Image.get = function(id) {
-  assert.equal(typeof id, 'string');
-  var self = this;
-  var tag = 'us.gcr.io/coderpad-1189/coderpad:' + id;
-  return Promise.fromNode(function(cb) {
-    docker.listImages(cb);
-  })
-  .filter(function(image) {
-    return _.includes(image.RepoTags, tag);
-  })
-  .then(function(images) {
-    assert(images.length < 2);
-    if (images.length === 1) {
-      return new Image({
-        id: id,
-        data: images[0]
-      });
-    } else {
-      throw new Error('Image not found: ' + id);
-    }
-  });
-}
-
-Image.prototype.version = function() {
-  var version = _.get(this.data, 'Labels[\'version\']');
-  if (!version) {
-    //throw new Error('Version label not found.');
-    version = 'Version label not found';
-  }
-  return Promise.resolve({
-    version: version
-  });
-}
-
-function Framework(opts) {
-  assert(typeof opts, 'object');
-  assert(typeof opts.id, 'string');
-  if (this instanceof Framework) {
-    this.opts = opts;
-    this.id = opts.id;
-  } else {
-    return new Framework(opts);
-  }
-}
-
-Framework.get = function(id) {
-  if (typeof id === 'string') {
-    return Promise.resolve(new Framework({id: id}));
-  } else if (typeof id === 'undefined') {
-    return Promise.resolve([
-      new Framework({id: 'c'}),
-      new Framework({id: 'javascript'}),
-      new Framework({id: 'scala'})
-    ]);
-  } else {
-    assert(false);
-  }
-}
-
-Framework.prototype.version = function() {
-  var url = _.get(config, 'languages[' + this.id + '].url');
-  if (!url) {
-    throw new Error('URL not found in config for ' + this.id);
-  }
-  var scraper = scrapers.WikiScraper({url: url});
-  return scraper.scrape();
-}
-
+// Init number of total languages being checked.
 var totalChecks = 0;
+
+// Init number of total languages that have failed results.
 var failedChecks = 0;
 
 // Get list of all languages.
-Language.getAll()
+languages.getAll()
+// Filter based on command line args.
+.filter(function(language) {
+  var search = argv._[0];
+  if (search) {
+    return _.startsWith(language.id, search);
+  } else {
+    return true;
+  }
+})
 // Set total number of checks.
 .tap(function(languages) {
   totalChecks = languages.length;
@@ -169,34 +42,36 @@ Language.getAll()
 // Iterate in series over each language.
 .mapSeries(function(language) {
   // Start.
-  console.log('\nChecking: ' + language.id);
-  return Promise.join(Image.get(language.id), Framework.get(language.id),
-  function(image, framework) {
-    return Promise.join(image.version(), framework.version(),
-    function(imageVersion, frameworkVersion) {
-      if (imageVersion.version === frameworkVersion.version) {
-        console.log(chalk.gray(util.format('%s = %s',
-          imageVersion.version,
-          frameworkVersion.version
-        )));
-        console.log(chalk.green('Status: OK'));
+  console.log('\nChecking ' + language.id);
+  // Get image object.
+  return images.get(language.id)
+  .then(function(image) {
+    // Get image and language versions.
+    return Promise.join(image.version(), language.version(),
+    function(imageVersion, languageVersion) {
+      if (imageVersion.version === languageVersion.version) {
+        // OK.
+        console.log(chalk.gray('image version:    ' + imageVersion.version));
+        console.log(chalk.gray('language version: ' + languageVersion.version));
+        console.log(chalk.green('Status:           OK'));
       } else {
+        // Failure.
         failedChecks += 1;
-        console.log(chalk.red(util.format('%s != %s',
-          imageVersion.version,
-          frameworkVersion.version
-        )));
-        console.log(chalk.red('Updated: ' + frameworkVersion.updated));
-        console.log(chalk.red('Status: Failed'));
+        console.log(chalk.red('image version:    ' + imageVersion.version));
+        console.log(chalk.red('language version: ' + languageVersion.version));
+        console.log(chalk.red('Updated:          ' + languageVersion.updated));
+        console.log(chalk.red('Status:           Failed'));
       }
     });
   })
+  // Handle errors.
   .catch(function(err) {
     failedChecks += 1;
     console.log(chalk.red('ERR: ' + err.message));
     console.log(chalk.red('ERR: ' + err.stack));
   });
 })
+// Report results.
 .then(function() {
   if (failedChecks === 0) {
     console.log(chalk.green(util.format(
@@ -210,5 +85,6 @@ Language.getAll()
       totalChecks
     )));
   }
+  // Exit the process.
   process.exit(failedChecks);
 });
